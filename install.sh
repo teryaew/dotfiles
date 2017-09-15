7adcb9d083d88b7d1c16b5159a4757a19ddd9fca
Mitya Teryaew <mitya.teryaew@gmail.com>
2017-09-15 14:47:07 +0300

    feat(*): add emmet preferences

--- install.sh
+++ install.sh
@@ -20,6 +20,7 @@ ln -sfv "$DOTFILES_DIR/runcom/.zshrc" ~
 ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
 ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
 ln -sfv "$DOTFILES_DIR/etc/.editorconfig" ~
+ln -sfv "$DOTFILES_DIR/etc/.emmet" ~
 
 # Package managers & packages
 

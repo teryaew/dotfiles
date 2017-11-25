9f35a993cbc80fdd497c36c5a360a9769c056795
Mitya Teryaew <mitya.teryaew@gmail.com>
2017-11-25 15:47:46 +0300

    feat(*): update rvm

--- install/gem.sh
+++ install/gem.sh
@@ -5,7 +5,8 @@ gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB
 
 source ~/.rvm/scripts/rvm
 
-rvm install 2.3.1
-rvm use 2.3.1 --default
+rvm install 2.4.2
+rvm use 2.4.2 --default
 
+gem cleanup
 gem install iStats
